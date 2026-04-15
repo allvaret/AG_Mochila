aae4cf01a1b06ac1e698fc298558ecedbf536007
Alvaro <alvarodanko032@gmail.com>
2026-04-15 18:31:11 -0300

    Commit inicial

---
+++ cromossomo.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Cromossomo:
+    nome_item: str
+    peso_item: int
+    valor_item: int
+    qtd_item: int
+
+    def __init__(self, nome_item, peso_item, valor_item, qtd_item):
+        self.nome_item = nome_item
+        self.peso_item = peso_item
+        self.valor_item = valor_item
+        self.qtd_item = qtd_item
