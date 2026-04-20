e92e03383bf37294689652798a937635226e761b
Alvaro <alvarodanko032@gmail.com>
2026-04-20 18:44:31 -0300

    Criacao da classe Individuo, que armazena as informacoes do algoritmo; Funcoes para criar e validar os cromossomos criados por outras funcoes

---
+++ individuo.py
@@ -0,0 +1,48 @@
+
+from random import randint
+
+
+class Individuo:
+    def __init__(self, cromossomo, limitacoes, peso_limite):
+        self.cromossomo = cromossomo
+        self.limitacoes = limitacoes
+        self.peso_limite = peso_limite
+        self.peso_total = sum(qtd * peso.peso_item for qtd, peso in zip(cromossomo, limitacoes))
+        self.valor_total = sum(qtd * valor.valor_item for qtd, valor in zip(cromossomo, limitacoes))
+        self.valido = self._validar()
+
+    def _validar(self):
+        """Verifica se o indivíduo é válido baseado no peso total"""
+        return self.peso_total <= self.peso_limite
+    
+    def reparar(self, max_tentativas=1000):
+        """Repara um indivíduo inválido gerando um novo válido"""
+        tentativas = 0
+        
+        while not self.valido and tentativas < max_tentativas:
+            novo_cromossomo = [randint(0, self.limitacoes[i].qtd_item) 
+                            for i in range(len(self.cromossomo))]
+            self.cromossomo = novo_cromossomo
+            self.valido = self._validar()
+            tentativas += 1
+        
+        if not self.valido:
+            raise ValueError(f"Não foi possível gerar indivíduo válido após {max_tentativas} tentativas")
+        
+        return self
+    
+    @classmethod
+    def criar_valido(cls, limitacoes, peso_limite, max_tentativas=1000):
+        """Factory method: cria um indivíduo já válido"""
+        for _ in range(max_tentativas):
+            cromossomo = [randint(0, item.qtd_item) for item in limitacoes]
+            individuo = cls(cromossomo, limitacoes, peso_limite)
+            
+            if individuo.valido:
+                return individuo
+        
+        raise ValueError(f"Não foi possível criar indivíduo válido após {max_tentativas} tentativas")
+    
+    def __repr__(self):
+        """Representação amigável para debug"""
+        return f"Individuo(cromossomo={self.cromossomo}, válido={self.valido}, peso={self.peso_total}, valor={self.valor_total})"
